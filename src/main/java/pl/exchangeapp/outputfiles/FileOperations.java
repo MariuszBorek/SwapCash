2b5efc4136bd9047adde4fa04874300d598fd081
Mariusz Borek <mariuszborek242@gmail.com>
2020-12-24 15:37:50 +0100

    added date and unique number to transactional history file name

--- src/main/java/pl/exchangeapp/outputfiles/FileOperations.java
+++ src/main/java/pl/exchangeapp/outputfiles/FileOperations.java
@@ -1,12 +1,15 @@
 package pl.exchangeapp.outputfiles;
 
 import java.io.FileWriter;
+import java.time.LocalDate;
+import java.util.UUID;
 
 public class FileOperations {
 
     public void saveTransactionHistoryToFile(String data) {
+
         try {
-            FileWriter fw = new FileWriter("savetransactions/transactionshistory.txt");
+            FileWriter fw = new FileWriter("savetransactions/"+ LocalDate.now() +"_transactionshistory_" + UUID.randomUUID() +".txt");
             fw.write(data);
             fw.close();
         } catch (Exception e) {
